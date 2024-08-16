0ac16ce6403fb0aca3fcbd220a29a803d70c34c8
I539173 <rohit.ravichandran@sap.com>
2024-08-16 10:11:10 +0530

    Testing regexp for Day 2

--- 2023/3.js
+++ 2023/3.js
@@ -6,9 +6,13 @@ cubes_req.set("red", 12).set("green", 13).set("blue", 14);
 
 const input_path = path.join(__dirname, "3-4.txt");
 
+// Regexp for matching a line
+// const game_id_regex = /Game (\d+): (( ?[\d]+ (red|blue|green),?)+;?)+/g;
+
 fs.readFile(input_path, { encoding: "utf-8" }).then((data) => {
     lines = data.split("\n");
     let game_id_sum = lines.reduce((acc, cur_line) => {
+        let results = cur_line.match(game_id_regex);
         let [game, cubes] = cur_line.split(":");
         cubes = cubes.trim().split(";");
         let game_id = parseInt(game.split(" ")[1]);
