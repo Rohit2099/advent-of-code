8d68595eac8d56f45801fcf1cf04ea3b088b6fba
I539173 <rohit.ravichandran@sap.com>
2024-09-09 17:10:16 +0530

    day 9 part 2

---
+++ 2023/9-2.js
@@ -0,0 +1,46 @@
+const { lstatSync } = require("fs");
+const fs = require("fs/promises");
+const path = require("path");
+
+const input_path = path.resolve(__dirname, "9.txt");
+
+function getExtrapolation(history) {
+    let oldHistory = history;
+    let firstValues = [];
+    while (true) {
+        if (Math.min(...oldHistory) === 0 && Math.max(...oldHistory) === 0) {
+            break;
+        }
+        let newHistory = [];
+        for (let i = 1; i < oldHistory.length; i++) {
+            newHistory.push(oldHistory[i] - oldHistory[i - 1]);
+        }
+        firstValues.push(oldHistory[0]);
+        oldHistory = newHistory;
+    }
+
+    let nextValue = 0;
+    let prevValue = 0;
+    for (let i = firstValues.length - 1; i >= 0; i--) {
+        nextValue = firstValues[i] - prevValue;
+        prevValue = nextValue;
+    }
+
+    return nextValue;
+}
+
+fs.readFile(input_path, { encoding: "utf-8" }).then((data) => {
+    let lines = data.trim().split("\n");
+    let history = lines.map((line) => {
+        return line
+            .trim()
+            .split(" ")
+            .map((val) => parseInt(val, 10));
+    });
+
+    let result = history.reduce((acc, curHistory) => {
+        return acc + getExtrapolation(curHistory);
+    }, 0);
+
+    console.log(result);
+});
