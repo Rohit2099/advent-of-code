6e9410d6459e58c2f52959f98ba509904516d08c
I539173 <rohit.ravichandran@sap.com>
2024-09-14 11:59:12 +0530

    outer fill and then calculate difference as inner points

--- 2023/10-2.js
+++ 2023/10-2.js
@@ -27,21 +27,21 @@ function bfs(maze, rowStart, colStart, inputComingFrom) {
         j = colStart,
         comingFrom = inputComingFrom;
     let visited = getNew2DArray(maze.length, maze[0].length);
-    let path = [];
+    let path = {};
     while (true) {
         let valid = i >= 0 && i < maze.length && j >= 0 && j < maze[0].length;
         if (!valid) {
             return undefined;
         }
         if (maze[i][j] === "S") {
-            path.push([i, j]);
+            path[`(${i}, ${j})`] = true;
             return path;
         }
         if (visited[i][j]) {
             return undefined;
         }
         visited[i][j] = true;
-        path.push([i, j]);
+        path[`(${i}, ${j})`] = true;
 
         switch (maze[i][j]) {
             case ".":
@@ -129,38 +129,62 @@ function findMazePath(maze, startPos) {
     return mazePath;
 }
 
-function findAreaFromPath(maze, mazePath) {
-    let rowToColMap = {};
-    for (let i = 0; i < mazePath.length; ++i) {
-        if (!rowToColMap[mazePath[i][0]]) {
-            rowToColMap[mazePath[i][0]] = [];
-        }
-        rowToColMap[mazePath[i][0]].push(mazePath[i][1]);
+function callBFSFill(mazePath, visited, i, j, rowMax, colMax, outerPoints) {
+    if (i < 0 || i >= rowMax || j < 0 || j >= colMax) {
+        return 0;
     }
-
-    const convertToInt = (a, b) => {
-        if (parseInt(a) > parseInt(b)) {
-            return 1;
-        } else {
-            return -1;
-        }
-    };
-
-    let area = 0;
-    for (let rows in rowToColMap) {
-        let cols = rowToColMap[rows];
-        cols.sort(convertToInt);
-
-        for (let i = 0; i + 1 < cols.length; i += 2) {
-            let start = cols[i],
-                end = cols[i + 1];
-            area += end - start - 1;
-        }
+    if (mazePath[`(${i}, ${j})`] || visited[i][j]) {
+        return 0;
     }
-    return area;
+    outerPoints[`(${i}, ${j})`] = true;
+    visited[i][j] = true;
+    return (
+        1 +
+        callBFSFill(mazePath, visited, i + 1, j, rowMax, colMax, outerPoints) +
+        callBFSFill(mazePath, visited, i - 1, j, rowMax, colMax, outerPoints) +
+        callBFSFill(mazePath, visited, i, j + 1, rowMax, colMax, outerPoints) +
+        callBFSFill(mazePath, visited, i, j - 1, rowMax, colMax, outerPoints)
+    );
 }
 
-function drawMazePath(maze, mazePath) {
+function findAreaOutsideLoop(maze, mazePath) {
+    let visited = getNew2DArray(maze.length, maze[0].length);
+    let outerRegion = 0;
+    let n = maze.length;
+    let m = maze[0].length;
+    let outerPoints = {};
+    for (let j = 0; j < m; ++j) {
+        if (visited[0][j] || mazePath[`(0, ${j}`]) {
+            continue;
+        }
+        outerRegion += callBFSFill(mazePath, visited, 0, j, n, m, outerPoints);
+    }
+    console.log(outerRegion);
+    for (let j = 0; j < m; ++j) {
+        if (visited[n - 1][j] || mazePath[`(${n - 1}, ${j}`]) {
+            continue;
+        }
+        outerRegion += callBFSFill(mazePath, visited, n - 1, j, n, m, outerPoints);
+    }
+    console.log(outerRegion);
+    for (let j = 0; j < n; ++j) {
+        if (visited[j][0] || mazePath[`(${j}, 0`]) {
+            continue;
+        }
+        outerRegion += callBFSFill(mazePath, visited, j, 0, n, m, outerPoints);
+    }
+    console.log(outerRegion);
+    for (let j = 0; j < m; ++j) {
+        if (visited[j][m - 1] || mazePath[`(${j}, ${m - 1}`]) {
+            continue;
+        }
+        outerRegion += callBFSFill(mazePath, visited, j, m - 1, n, m, outerPoints);
+    }
+    console.log(outerRegion);
+    return outerPoints;
+}
+
+function drawMazePath(maze, mazePath, outerPoints) {
     let newMaze = new Array(maze.length);
     for (let i = 0; i < maze.length; ++i) {
         newMaze[i] = new Array(maze[i].length);
@@ -168,13 +192,23 @@ function drawMazePath(maze, mazePath) {
             newMaze[i][j] = " ";
         }
     }
-    for (let i = 0; i < mazePath.length; ++i) {
-        newMaze[mazePath[i][0]][mazePath[i][1]] = ".";
+    for (let entry in mazePath) {
+        let [_, i, j] = entry.match(/\((\d+), (\d+)\)/);
+        i = parseInt(i);
+        j = parseInt(j);
+        newMaze[i][j] = ".";
+    }
+
+    for (let entry in outerPoints) {
+        let [_, i, j] = entry.match(/\((\d+), (\d+)\)/);
+        i = parseInt(i);
+        j = parseInt(j);
+        newMaze[i][j] = "X";
     }
 
     let finalString = "";
     for (let i = 0; i < newMaze.length; ++i) {
-        finalString += newMaze[i].join(" ") + "\n";
+        finalString += newMaze[i].join("") + "\n";
     }
     fs.writeFile("10-out.txt", finalString, { encoding: "utf-8" });
 }
@@ -186,7 +220,13 @@ fs.readFile(input_path, { encoding: "utf-8" }).then((data) => {
     });
     const startPos = findStartPos(maze);
     let mazePath = findMazePath(maze, startPos);
-    drawMazePath(maze, mazePath);
-    const area = findAreaFromPath(maze, mazePath);
-    console.log(area);
+    const outerPoints = findAreaOutsideLoop(maze, mazePath);
+    drawMazePath(maze, mazePath, outerPoints);
+    // const totalPoints = maze.length * maze[0].length;
+    // const loopPoints = Object.keys(mazePath).length;
+    // const innerPoints = totalPoints - outerArea - loopPoints;
+    // console.log(`Maze Length: ${loopPoints}`);
+    // console.log(`Total points: ${totalPoints}`);
+    // console.log(`Outer Area: ${outerArea}`);
+    // console.log(`Inner points: ${innerPoints}`);
 });
