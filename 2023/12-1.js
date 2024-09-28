db51fb4065c299f9bda15d4811c5637322e6e8fa
I539173 <rohit.ravichandran@sap.com>
2024-09-29 02:20:17 +0530

    day 12 part 1

--- 2023/12-1.js
+++ 2023/12-1.js
@@ -3,46 +3,72 @@ const path = require("path");
 
 const input_path = path.join(__dirname, "12.txt");
 
-function callRec(condition, result, resultIdx, val, conditionIdx, count) {
+function callRec(condition, result, resultIdx, val, conditionIdx, output, prevHash) {
     if (resultIdx >= result.length && conditionIdx >= condition.length) {
-        count.count += 1;
-        return true;
-    } else if (resultIdx >= result.length || conditionIdx >= condition.length) {
-        return false;
+        let resultStr = result.join(", ");
+        let record = condition + "  " + resultStr;
+        output.count += 1;
+        output.permutations.push(record);
+        return;
+    } else if (resultIdx >= result.length) {
+        for (let i = conditionIdx; i < condition.length; i++) {
+            if (condition[i] === "#") {
+                return;
+            }
+        }
+        output.count += 1;
+        let resultStr = result.join(", ");
+        let newCondition = condition.replaceAll("?", ".");
+        let record = newCondition + "  " + resultStr;
+        output.permutations.push(record);
+        return;
+    } else if (conditionIdx >= condition.length) {
+        return;
     }
 
-
     if (condition[conditionIdx] === ".") {
-        return callRec(condition, result, resultIdx, val, conditionIdx + 1, count);
+        let started = val < result[resultIdx];
+        if (started) {
+            return;
+        }
+        callRec(condition, result, resultIdx, val, conditionIdx + 1, output, false);
     }
     if (condition[conditionIdx] === "?") {
         let started = val < result[resultIdx];
-        var useDot = false;
         if (!started) {
-            var useDot = callRec(condition, result, resultIdx, val, conditionIdx + 1, count);
+            let newCondition =
+                condition.slice(0, conditionIdx) + "." + condition.slice(conditionIdx + 1);
+            callRec(newCondition, result, resultIdx, val, conditionIdx + 1, output, false);
         }
-        let newResultIdx = val - 1 === 0 ? resultIdx + 1 : resultIdx;
-        let newVal = val - 1 === 0 ? result[newResultIdx] : val - 1;
-        let useHash = callRec(condition, result, newResultIdx, newVal, conditionIdx + 1, count);
-
-        return useDot || useHash;
+        if (started || (!started && !prevHash)) {
+            let newResultIdx = val - 1 === 0 ? resultIdx + 1 : resultIdx;
+            let newVal = val - 1 === 0 ? result[newResultIdx] : val - 1;
+            let newCondition =
+                condition.slice(0, conditionIdx) + "#" + condition.slice(conditionIdx + 1);
+            callRec(newCondition, result, newResultIdx, newVal, conditionIdx + 1, output, true);
+        }
     }
     if (condition[conditionIdx] === "#") {
-        if (val < result[resultIdx] && result[resultIdx - 1] === ){
-            
+        let started = val < result[resultIdx];
+        if (started || (!started && !prevHash)) {
+            let newResultIdx = val - 1 === 0 ? resultIdx + 1 : resultIdx;
+            let newVal = val - 1 === 0 ? result[newResultIdx] : val - 1;
+            callRec(condition, result, newResultIdx, newVal, conditionIdx + 1, output, true);
         }
-        let newResultIdx = val - 1 === 0 ? resultIdx + 1 : resultIdx;
-        let newVal = val - 1 === 0 ? result[newResultIdx] : val - 1;
-        return callRec(condition, result, newResultIdx, newVal, conditionIdx + 1, count);
     }
-
-    return false;
 }
 
 function getNumberOfArrangements(record) {
-    let count = { count: 0 };
-    callRec(record.condition, record.result, 0, record.result[0], 0, count);
-    return count.count;
+    let output = { count: 0, permutations: [] };
+    callRec(record.condition, record.result, 0, record.result[0], 0, output);
+    return output;
+}
+
+function printPermutations(permutations) {
+    fs.writeFile("12-out.txt", "").then(() => {
+        let strPermutations = permutations.join("\n") + "\n";
+        fs.writeFile("12-out.txt", strPermutations, { flag: "a+" });
+    });
 }
 
 fs.readFile(input_path, { encoding: "utf-8" }).then((data) => {
@@ -59,9 +85,16 @@ fs.readFile(input_path, { encoding: "utf-8" }).then((data) => {
         };
     });
 
-    const result = records.reduce((acc, record) => {
-        return acc + getNumberOfArrangements(record);
-    }, 0);
+    const result = records.reduce(
+        (acc, record) => {
+            let res = getNumberOfArrangements(record);
+            acc.count += res.count;
+            acc.permutations.push(...res.permutations);
+            return acc;
+        },
+        { count: 0, permutations: [] }
+    );
 
-    console.log(result);
+    printPermutations(result.permutations);
+    console.log(result.count);
 });
